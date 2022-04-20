497c68716d95c81fc8de4d1a11e29057e2ea5735
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-19 23:13:39 -0500

    [FIX] for empty list or not a list

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,29 +6,30 @@ def canUnlockAll(boxes):
     ''' Method to check if list contain right secuence to open all boxes '''
     cajas = []
     abiertas = [0]
-    total = 0
+
+    if not boxes or not isinstance(boxes, list):
+        return False
+
     for h in boxes:
         cajas.append(1)
 
     cajas[0] = 0
 
-    for i in cajas:
-        total = total + i
-
-    if abrircaja(boxes[0], boxes, cajas, abiertas, total):
+    if abrircaja(boxes[0], boxes, cajas, abiertas):
         return True
     return False
 
 
-def abrircaja(p, boxes, cajas, abiertas, total):
+def abrircaja(p, boxes, cajas, abiertas):
     ''' Recursive check for opened boxes '''
     nuevas = p
+
     for s in nuevas:
-        cajas[s] = 0
-        if s < total:
+        if s < len(boxes):
+            cajas[s] = 0
             if s not in abiertas:
                 abiertas.append(s)
-                abrircaja(boxes[s], boxes, cajas, abiertas, total)
+                abrircaja(boxes[s], boxes, cajas, abiertas)
 
     if sum(cajas) == 0:
         return True
