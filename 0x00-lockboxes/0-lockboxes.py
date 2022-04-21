473c90899a27ebfa0e9ffa8ec7f6c1ef20172a94
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-21 12:09:51 -0500

    [CHANGE] remove recursion use for inside for

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,34 +3,17 @@
 
 
 def canUnlockAll(boxes):
-    """ Method to check if list contain right secuence to open all boxes """
-    cajas = []
+    """ Method to check if list contain right sequence to open all boxes """
     abiertas = [0]
 
     if not boxes or not isinstance(boxes, list):
         return False
 
-    for h in boxes:
-        cajas.append(1)
+    for h, k in enumerate(boxes):
+        for x in boxes[h]:
+            if x not in abiertas and x < len(boxes):
+                abiertas.append(x)
 
-    cajas[0] = 0
-
-    if abrircaja(boxes[0], boxes, cajas, abiertas):
-        return True
-    return False
-
-
-def abrircaja(p, boxes, cajas, abiertas):
-    """ Recursive check for opened boxes """
-    nuevas = p
-
-    for s in nuevas:
-        if s < len(boxes):
-            cajas[s] = 0
-            if s not in abiertas:
-                abiertas.append(s)
-                abrircaja(boxes[s], boxes, cajas, abiertas)
-
-    if sum(cajas) == 0:
+    if len(abiertas) == len(boxes):
         return True
     return False
