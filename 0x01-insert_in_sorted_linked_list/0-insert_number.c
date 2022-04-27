4c02815633986f28efb46b7b8755eccf36de8cd5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-26 22:13:47 -0500

    [UPDATE]

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	// define vars
+	// malloc
+	// walk nodes until next is mayor
+	// disconect nodes
+	// connect new node to prev
+	// connect next to freed node in chain
+}
