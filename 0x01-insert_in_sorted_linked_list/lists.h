804840bf06a1bffb024217cd214ff0c7c210e091
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-26 22:07:59 -0500

    [UPDATE]

--- 0x01-insert_in_sorted_linked_list/lists.h
+++ 0x01-insert_in_sorted_linked_list/lists.h
@@ -1,6 +1,22 @@
 #ifndef _INS_SLL_
 #define _INS_SLL_
 
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 listint_t *insert_node(listint_t **head, int number);
 
 #endif
