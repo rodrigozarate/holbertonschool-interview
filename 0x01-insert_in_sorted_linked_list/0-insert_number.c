abbf48240daad65068104eab11c4e17c6ae4583f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-27 21:26:35 -0500

    [TEST] change bucle to remove flag

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -11,11 +11,10 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *newnode, *nodeposition = *head;
-	int flag = 0;
 
-	newnode = malloc(sizeof(listint_t));
-		if (newnode == NULL)
-			return (NULL);
+	newnode = malloc(sizeof(*newnode));
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = number;
 
 	if (*head == NULL || (*head)->n > number)
@@ -24,22 +23,14 @@ listint_t *insert_node(listint_t **head, int number)
 		*head = newnode;
 		return (newnode);
 	}
-	while (nodeposition->next != NULL)
+	for (; nodeposition != NULL ; nodeposition = nodeposition->next)
 	{
-		if (nodeposition->next->n > number)
+		if (nodeposition->next == NULL || nodeposition->next->n > number)
 		{
 			newnode->next = nodeposition->next;
 			nodeposition->next = newnode;
-			return (newnode);
-			flag = 1;
+			break;
 		}
-		nodeposition = nodeposition->next;
-	}
-	if (flag == 0)
-	{
-		newnode->next = nodeposition->next;
-		nodeposition->next = newnode;
-		return (newnode);
 	}
 	return (newnode);
 }
