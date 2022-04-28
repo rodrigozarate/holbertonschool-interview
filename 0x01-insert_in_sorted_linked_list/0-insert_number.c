dcd1802e22f4f30b11ae46121d587da7ec12a367
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-27 19:22:01 -0500

    [UPDATE]

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,12 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
-
+/**
+ * insert_node - insert a new node
+ * @head: double pointer
+ * @n: int of the list
+ * Return: pointer of newnode or NULL if fail
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-	// define vars
-	// malloc
-	// walk nodes until next is mayor
-	// disconect nodes
-	// connect new node to prev
-	// connect next to freed node in chain
+	listint_t *newnode;
+	listint_t *nodeposition;
+	listint_t *nodeprev;
+
+	nodeposition = *head;
+
+	newnode = malloc(sizeof(listint_t));
+		if (newnode == NULL)
+			return(NULL);
+	newnode->n = number;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+		printf("here i m \n");
+	}
+	while (nodeposition->next != NULL)
+	{
+		printf("pre got n: %d \n", nodeposition->n);
+		
+		/* walk nodes until next is mayor */
+		if (number > nodeposition->n)
+		{
+			printf("mayor n: %d \n", nodeposition->n);
+			nodeprev = nodeposition;
+			nodeposition = nodeposition->next;
+			printf("post mayor n: %d \n", nodeposition->n);	
+		}
+		else
+		{
+			/* disconect nodes */
+			newnode->next = nodeposition;
+			/* connect next to freed node in chain */
+			nodeprev->next = newnode;
+			return(newnode);
+		}
+		/* connect new node to prev */
+		printf("got n: %d \n", nodeposition->n);
+		
+	}
+	return(newnode);
 }
--- 0x01-insert_in_sorted_linked_list/lists.h
+++ 0x01-insert_in_sorted_linked_list/lists.h
@@ -1,5 +1,5 @@
-#ifndef _INS_SLL_
-#define _INS_SLL_
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stddef.h>
 
@@ -17,6 +17,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 listint_t *insert_node(listint_t **head, int number);
 
-#endif
+#endif /* LISTS_H */
