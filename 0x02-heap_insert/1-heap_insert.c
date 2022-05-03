7dec776dbd53bf8f43c984a6c4897d7dcf8b77a4
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-05-03 11:22:57 -0500

    [UPDATE]

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,2 +1,14 @@
-
-oasidjlsajdlsajd
+#include binary_trees.h
+heap_t *heap_insert(heap_t **root, int value)
+{
+	/* if adress is null make root */
+	if (*root == NULL)
+	{
+		*root = binary_tree_node(*root, value);
+		return (*root);
+	}
+	/* check for place to put the node according to MAX heap */
+	/* validate if node must be swaped */
+	/* validate if node is leaf */
+	/* return new node */
+}
