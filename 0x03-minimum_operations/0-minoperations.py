7b314210af8ab5df157cb1c35795124f8e82e027
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-05-10 02:20:53 -0500

    [CREATE]

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module to calcule min number of operations """
+
+
+def minOperations(n):
+    """ min number of ops """
+    """ Check for <= 1 that will require 0 ops """
+    """ Check for odd or even """
+    """ Pairs at least can have one copy all """
+    """ Check for prime numbers """
+    """ Check for imposibe number """
+    """ Paste can be thinked as divided by 2 """
+    
+
+
