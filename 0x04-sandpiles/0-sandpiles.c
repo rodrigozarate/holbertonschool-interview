6158c4723d38c3688f437a7679f3ac417658e179
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-05-17 01:05:25 -0500

    [CREATE]

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,11 @@
+/*
+* Author: Rodrigo Zarate
+* Date: may 17 2022
+*/
+
+#include "sandpiles.h"
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	/* code */
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,6 @@
+#ifndef _SANDPILES_
+#define _SANDPILES_
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* _SANDPILES_ */
