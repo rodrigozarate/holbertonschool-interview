81d272e7aa88eda8efd0a9146003794befe1eff4
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-05-18 22:50:23 -0500

    [FIX] betty, split functions, lines

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -5,10 +5,17 @@
 
 #include "sandpiles.h"
 
+/**
+ * sandpiles_sum - sum sandpiles
+ * @grid1: stable grid
+ * @grid2: sum grid
+ *
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
 	int flag = 0;
+
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -16,73 +23,75 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 			grid1[i][j] = grid1[i][j] + grid2[i][j];
 		}
 	}
-	printf("gato\n");
 	flag = validate(grid1);
-	printf("fin gato\n");
 
 	while (flag)
 	{
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (grid1[i][j] >= 4)
-				{
-					grid1[i][j] = grid1[i][j] - 4;
-					grid2[i][j] = 1;
-				}
-				else
-				{
-					grid2[i][j] = 0;
-				}
-			}
-		}
-		printf(" resta \n");
-		printf("grid1:\n");
-		own_print_grid(grid1);
-		printf("grid2:\n");
-		own_print_grid(grid2);
-		printf(" resta \n");
-
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (grid2[i][j] == 1)
-				{
-					if (i - 1 >= 0)
-					{
-						printf("bottom -");
-						grid1[i - 1][j]++;
-					}
-
-					if (j - 1 >= 0)
-					{
-						printf("right -");
-						grid1[i][j - 1]++;
-					}
-					if (i + 1 < 3)
-					{
-						printf("up -");
-						grid1[i + 1][j]++;
-					}
-
-					if (j + 1 < 3)
-					{
-						printf("left -");
-						grid1[i][j + 1]++;
-					}
-				}
-			}
-		}
-		printf(" suma \n");
-		printf("grid1 ---\n");
-		own_print_grid(grid1);
-		printf("grid2 ---\n");
-		own_print_grid(grid2);
-		printf(" suma \n");
-
+		reduce(grid1, grid2);
+		distribute(grid1, grid2);
 		flag = validate(grid1);
+	}
+}
+
+/**
+ * reduce - reduce grid
+ * @grid1: grid stable
+ * @grid2: control
+ */
+void reduce(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] >= 4)
+			{
+				grid1[i][j] = grid1[i][j] - 4;
+				grid2[i][j] = 1;
+			}
+			else
+			{
+				grid2[i][j] = 0;
+			}
+		}
+	}
+}
+
+/**
+ * distribute - distribute grid
+ * @grid1: grid stable
+ * @grid2: control
+ */
+void distribute(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid2[i][j] == 1)
+			{
+				if (i - 1 >= 0)
+				{
+					grid1[i - 1][j]++;
+				}
+				if (j - 1 >= 0)
+				{
+					grid1[i][j - 1]++;
+				}
+				if (i + 1 < 3)
+				{
+					grid1[i + 1][j]++;
+				}
+				if (j + 1 < 3)
+				{
+					grid1[i][j + 1]++;
+				}
+			}
+		}
 	}
 }
 
@@ -106,31 +115,31 @@ int validate(int grid[3][3])
 			}
 		}
 	}
-	printf("--------------------------------\n");
-	own_print_grid(grid);
-	printf("--------------------------------\n");
+	if (flag)
+	{
+		own_print_grid(grid);
+	}
 	return (flag);
 }
 
 /**
- * print_grid - Print 3x3 grid
+ * own_print_grid - Print 3x3 grid
  * @grid: 3x3 grid
- *
  */
 void own_print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
 	printf("=\n");
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
 		printf("\n");
-    }
+	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,8 +5,8 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void own_print_grid(int grid[3][3]);
-void reduce(int grid[3][3]);
-void distribute(int grid[3][3], int i, int j);
+void reduce(int grid1[3][3], int grid2[3][3]);
+void distribute(int grid1[3][3], int grid2[3][3]);
 int validate(int grid[3][3]);
 
 #endif /* _SANDPILES_ */
