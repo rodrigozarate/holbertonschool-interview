a85d3f2d4be86bf1961214acad7c97aa80257a79
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-05-23 19:18:59 -0500

    [UPDATE] ad function count_nodes

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,4 +1,25 @@
 #include "lists.h"
 
 int is_palindrome(listint_t **head)
+{
+	/* count nodes to determine odd or even */
+	int n = 0;
+	n = count_nodes(*head);
+	return (n);
 
+	/* insert nodes in head to produce a reverse list */
+	/* traverse nodes and compare if both values are equal */
+}
+
+int count_nodes(listint_t *head)
+{
+	unsigned int n = 0;
+
+	while(head)
+	{
+		head = head->next;
+		n++;
+	}
+	printf("nodes: %d", n);
+	return (n);
+}
