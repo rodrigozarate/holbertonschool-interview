9f7f26d2c72171cd3b3074a2c852c1df9b725ed5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-06 10:58:41 -0500

    [UPDATE]

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,3 +1,32 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if theres a cycle
+ * @list: pointer
+ * Return: 0 or 1 if cycle
+ */
 int check_cycle(listint_t *list)
 {
+	listint_t *by1, *by2;
+
+	if (!list || !(list->next))
+		return (0);
+
+	by1 = list;
+	by2 = list->next;
+
+	while (by1 && by1->next && by2 && by2->next)
+	{
+		if (by1 == by2)
+		{
+			return (1);
+		}
+		else
+		{
+			by1 = by1->next;
+			by2 = by2->next->next;
+		}
+	}
+
+	return (0);
 }
