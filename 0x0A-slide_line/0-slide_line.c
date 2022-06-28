4a88a791caecb8708217762009f30645cba7558d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-28 11:04:22 -0500

    [UPDATE]

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,4 +1,30 @@
 #include "slide_line.h"
+
+/**
+* slide_line_left - slide and merge to the left
+* @line: array
+* @size: size of the array
+*/
+static void slide_line_left(int *line, size_t size)
+{
+        /* walk array */
+        /* move ints to desplace zeros */
+        /* if pair of equal ints merge */
+        /* displace again and wait */
+}
+
+/**
+* slide_line_right - slide and merge to the right
+* @line: array
+* @size: size of the array
+*/
+slide_line_right(line, size)
+{
+	/* walk array */
+	/* move ints to desplace zeros */
+	/* if pair of equal ints merge */
+	/* displace again and wait */
+}
 /**
 * slide_line - mock 2048 game behavior
 * @line: pointer
@@ -18,10 +44,10 @@ int slide_line(int *line, size_t size, int direction)
 		return (1);
 
 	if (direction == SLIDE_LEFT)
-		slide_line_left();
+		slide_line_left(line, size);
 
 	if (direction == SLIDE_RIGHT)
-		slide_line_right();
+		slide_line_right(line, size);
 
 	return (1);
 }
