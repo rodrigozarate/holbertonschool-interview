65caaa7bf0a73026bc6744a7573b55a23d989a90
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-28 10:27:28 -0500

    [UPDATE]

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -3,13 +3,25 @@
 * slide_line - mock 2048 game behavior
 * @line: pointer
 * @size: size of the array
-* @direction: L or R
+* @direction: L = 0 or R = 1
 * Return: True or False
 */
 int slide_line(int *line, size_t size, int direction)
 {
-	// chack if params are ok
-	// check if line can be slided
-	// check if line can be merged
-	return True;
+	if (line == NULL || size == 0)
+		return (0);
+
+	if (direction != SLIDE_LEFT || direction != SLIDE_RIGHT)
+		return (0);
+
+	if ((int)size == 1)
+		return (1);
+
+	if (direction == SLIDE_LEFT)
+		slide_line_left();
+
+	if (direction == SLIDE_RIGHT)
+		slide_line_right();
+
+	return (1);
 }
