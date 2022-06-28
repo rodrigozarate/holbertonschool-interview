041ee457a8ba73a9aba4d64de48bb17f8343be3c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-28 10:10:34 -0500

    [UPDATE]

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,15 @@
+#include "slide_line.h"
+/**
+* slide_line - mock 2048 game behavior
+* @line: pointer
+* @size: size of the array
+* @direction: L or R
+* Return: True or False
+*/
+int slide_line(int *line, size_t size, int direction)
+{
+	// chack if params are ok
+	// check if line can be slided
+	// check if line can be merged
+	return True;
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,9 @@
+#ifndef _MOCK_2048_
+#define _MOCK_2048_
+
+#define SLIDE_LEFT L
+#define SLIDE_RIGHT R
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* _MOCK_2048_
