3d701b5788fb004af3f8ee58cd9d76603499d3ec
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-28 20:47:12 -0500

    [UPDATE]

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,16 +1,58 @@
+#include <stdio.h>
+#include <stddef.h>
 #include "slide_line.h"
 
 /**
-* slide_line_left - slide and merge to the left
+* slide_line_left - slide to the left
 * @line: array
 * @size: size of the array
 */
 static void slide_line_left(int *line, size_t size)
 {
-        /* walk array */
-        /* move ints to desplace zeros */
-        /* if pair of equal ints merge */
-        /* displace again and wait */
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	for (;i < (int)size; i++)
+	{
+		if (line[i] == 0)
+		{
+			j++;
+		}
+		else
+		{
+			line[k] = line[j];
+			if (k != j)
+			{
+				line[j] = 0;
+			}
+			j++;
+			k++;
+		}
+	}
+}
+
+/**
+* merge_line_left - merge to the left
+* @line: array
+* @size: size of the array
+*/
+static void merge_line_left(int *line, size_t size)
+{
+	int i = 0;
+
+	for (;i < (int)size; i++)
+	{
+		if (i + 1 < (int)size)
+		{
+			if (line[i] == line[i + 1])
+			{
+				line[i] = line[i] * 2;
+				line[i + 1] = 0;
+			}
+		}
+	}
+	slide_line_left(line, size);
 }
 
 /**
@@ -18,13 +60,55 @@ static void slide_line_left(int *line, size_t size)
 * @line: array
 * @size: size of the array
 */
-slide_line_right(line, size)
+static void slide_line_right(int *line, size_t size)
 {
-	/* walk array */
-	/* move ints to desplace zeros */
-	/* if pair of equal ints merge */
-	/* displace again and wait */
+	int i = (int)size - 1;
+	int j = (int)size - 1;
+	int k = (int)size - 1;
+
+	for (;i >= 0; i--)
+	{
+		if (line[i] == 0)
+		{
+			j--;
+		}
+		else
+		{
+			line[k] = line[j];
+			if (k != j)
+                        {
+				line[j] = 0;
+			}
+			j--;
+			k--;
+		}
+	}
+
 }
+
+/**
+* merge_line_left - merge to the left
+* @line: array
+* @size: size of the array
+*/
+static void merge_line_right(int *line, size_t size)
+{
+	int i = (int)size - 1;
+
+	for (;i >= 0; i--)
+	{
+		if (i - 1 >= 0)
+		{
+			if (line[i] == line[i - 1])
+			{
+				line[i] = line[i] * 2;
+				line[i - 1] = 0;
+			}
+		}
+	}
+	slide_line_right(line, size);
+}
+
 /**
 * slide_line - mock 2048 game behavior
 * @line: pointer
@@ -37,17 +121,25 @@ int slide_line(int *line, size_t size, int direction)
 	if (line == NULL || size == 0)
 		return (0);
 
-	if (direction != SLIDE_LEFT || direction != SLIDE_RIGHT)
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
+	{
 		return (0);
+	}
 
 	if ((int)size == 1)
 		return (1);
 
 	if (direction == SLIDE_LEFT)
+	{
 		slide_line_left(line, size);
+		merge_line_left(line, size);
+	}
 
 	if (direction == SLIDE_RIGHT)
+	{
 		slide_line_right(line, size);
+		merge_line_right(line, size);
+	}
 
 	return (1);
 }
--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,9 +1,9 @@
 #ifndef _MOCK_2048_
 #define _MOCK_2048_
 
-#define SLIDE_LEFT L
-#define SLIDE_RIGHT R
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
 
 int slide_line(int *line, size_t size, int direction);
 
-#endif /* _MOCK_2048_
+#endif /* _MOCK_2048_ */
