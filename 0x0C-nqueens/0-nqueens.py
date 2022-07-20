a1002264fa33c3af7c45ba62655ddf8dbda94268
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-07-20 11:49:30 -0500

    [UPDATE]

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -2,7 +2,28 @@
 
 import sys
 
-if len(sys.argv) < 2:
-    print('minor')
-else:
-    print('ok')
+if len(sys.argv) is not 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print('N must be a number')
+    exit(1)
+
+N = int(sys.argv[1])
+
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+
+
+def queens(N, i, a, b, c, d):
+    if i < N:
+        for j in range(N):
+            if j not in a and i+j not in b and i-j not in c:
+                yield from queens(N, i+1, a+[j], b+[i+j], c+[i-j], d+[[i, j]])
+    else:
+        yield d
+
+for solution in queens(N, 0, [], [], [], []):
+    print(solution)
