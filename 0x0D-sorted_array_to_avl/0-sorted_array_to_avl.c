94f66b6de107c5d00ed61a835ac2bd9132d82e4d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-07-29 21:37:43 -0500

    [UPDATE]

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,6 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+
+/**
+* create_node - creates new node
+* @parent: pointer
+* @value: int value
+* Return: pointer to new node
+*/
 binary_tree_t *create_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
@@ -13,7 +24,14 @@ binary_tree_t *create_node(binary_tree_t *parent, int value)
 	return (new_node);
 }
 
-
+/**
+* avl_helper - define spots of values based on height
+* @array: array from which create avl
+* @lindex: left index
+* @rindex: right index
+* @parent: parent if any
+* Return: pointer
+*/
 avl_t *avl_helper(int *array, int lindex, int rindex, avl_t *parent)
 {
 	avl_t *root = NULL;
@@ -37,11 +55,16 @@ avl_t *avl_helper(int *array, int lindex, int rindex, avl_t *parent)
 }
 
 
-
+/**
+* sorted_array_to_avl - convert a sorted array into avl
+* @array: int array
+* @size: array size
+* Return: pointer
+*/
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	if (!array || size == 0)
 		return (NULL);
 
-	return(avl_helper(array, 0, (int)size - 1, NULL));
+	return (avl_helper(array, 0, (int)size - 1, NULL));
 }
