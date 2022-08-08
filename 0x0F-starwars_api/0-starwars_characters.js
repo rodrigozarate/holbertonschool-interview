d516c81932efe4a4008354c4069c49a33448ff1b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-08-08 02:59:58 -0500

    [UPDATE]

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -7,16 +7,23 @@ let film = process.argv[2];
 
 console.log(url+film);
 try {
-	request(url+film, function(error, response, body){
+	what = request(url+film, function(error, response, body){
 		if (!error && response.statusCode == 200) {
-			console.log(body);
+			
+			mico = JSON.parse(body);
+
+			for(character in mico["characters"]){
+				console.log(mico["characters"][character]);
+			}
 		}
 	});
 } catch (error) {
 	console.error(error);
 }
-// get the films endpoint
+// get the films endpoint OK
+
 // get the character list of urls
+
 // get each name from people endpoint
 // https://swapi-api.hbtn.io/api/people/1/
 // print
