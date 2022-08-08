2bfa416e240412a2780cc0bec9c601f4ab1329c5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-08-08 02:13:46 -0500

    [UPDATE]

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,7 +1,22 @@
-#!/usr/bin/node
+#!/Users/user/.nvm/versions/node/v10.14.0/bin/node
 
-get the films endpoint
-get the character list of urls
-get each name from people endpoint
-https://swapi-api.hbtn.io/api/people/1/
-print
+const request = require('request')
+
+let url = 'https://swapi-api.hbtn.io/api/films/';
+let film = process.argv[2];
+
+console.log(url+film);
+try {
+	request(url+film, function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+		}
+	});
+} catch (error) {
+	console.error(error);
+}
+// get the films endpoint
+// get the character list of urls
+// get each name from people endpoint
+// https://swapi-api.hbtn.io/api/people/1/
+// print
