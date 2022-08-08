d41b18e6c1d2effcb815eca98c349bb6de7bd038
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-08-08 04:02:27 -0500

    [FIX] Use async to maintan order

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,29 +5,42 @@ const request = require('request')
 let url = 'https://swapi-api.hbtn.io/api/films/';
 let film = process.argv[2];
 
-console.log(url+film);
-try {
-	what = request(url+film, function(error, response, body){
-		if (!error && response.statusCode == 200) {
-			
-			mico = JSON.parse(body);
+function whois (whom) {
+  return new Promise((resolve, reject) => {
+    request(whom, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body).name);
+    });
+  });
+}
 
-			for(character in mico["characters"]){
-				whom = mico["characters"][character];
-				who = request(whom, function(error, response, body){
-					loro = JSON.parse(body);
-					console.log(loro["name"]);
-				});
-			}
-		}
-	});
-} catch (error) {
-	console.error(error);
+async function charlist () {
+  try {
+
+    for(character in mico["characters"]){
+      whom = mico["characters"][character];
+      toprint = await whois(whom);
+      console.log(toprint);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 // get the films endpoint OK
 
 // get the character list of urls OK
 
-// get each name from people endpoint
+// get each name from people endpoint OK
+
+what = request(url+film, function(error, response, body){
+  if (!error && response.statusCode == 200) {
+
+        mico = JSON.parse(body);
+	charlist(mico);
+  }
+});
+
 // https://swapi-api.hbtn.io/api/people/1/
 // print
