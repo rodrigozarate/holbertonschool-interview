064cae43305b2d968cd291d435ca9ec8445c471d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-08-11 23:18:07 -0500

    [FIX] semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/Users/user/.nvm/versions/node/v10.14.0/bin/node
 
-const request = require('request')
+const request = require('request');
 
-let url = 'https://swapi-api.hbtn.io/api/films/';
-let film = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const film = process.argv[2];
 
 function whois (whom) {
   return new Promise((resolve, reject) => {
@@ -17,11 +17,11 @@ function whois (whom) {
 }
 
 async function charlist () {
+  const mico = [];
   try {
-
-    for(character in mico["characters"]){
-      whom = mico["characters"][character];
-      toprint = await whois(whom);
+    for (const character in mico.characters) {
+      const whom = mico.characters[character];
+      const toprint = await whois(whom);
       console.log(toprint);
     }
   } catch (error) {
@@ -29,10 +29,9 @@ async function charlist () {
   }
 }
 
-what = request(url+film, function(error, response, body){
-  if (!error && response.statusCode == 200) {
-
-        mico = JSON.parse(body);
-	charlist(mico);
+request(url + film, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const mico = JSON.parse(body);
+    charlist(mico);
   }
 });
