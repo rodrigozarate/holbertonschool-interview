1da7faa87c2bd4ae8f9e4e8ebf818c3fe14f490d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-08-18 22:06:31 -0500

    [FIX]

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,19 +4,19 @@
 
 def rain(walls):
     """ Check the ammount of water """
-    if type(walls) is not list
+    if type(walls) is not list:
         return 0
 
     wtr = 0
     wlps = 0
-    
+
     for i in range(0, len(walls) - 2):
         if walls[i] > 0 and i >= wlps:
             for j in range(i + 2, len(walls)):
                 if walls[j] >= walls[i]:
-                   wtrl = walls[i]
-                   for k in range(i + 1, j):
-                       wtr += wtrl - walls[k]
-                   wlps = j
-                   break
+                    wtrl = walls[i]
+                    for k in range(i + 1, j):
+                        wtr += wtrl - walls[k]
+                    wlps = j
+                    break
     return wtr
