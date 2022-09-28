163eb340943e78947fe40509f2f3754d5b586397
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-09-28 08:03:42 -0500

    [FIX] PEP8 space

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,6 +6,7 @@ Created on Mon Sep 26 20:15:15 2022
 @author: Rodrigo Zárate Algecira
 """
 
+
 def rotate_2d_matrix(matrix):
     """ Rotate matrix 90 degres clockwise """
     allmat = []
@@ -14,7 +15,6 @@ def rotate_2d_matrix(matrix):
         for j in range(len(matrix)):
             newmat.insert(0, matrix[j][i])
         allmat.append(newmat)
-    
+
     for i in range(len(matrix)):
         matrix[i] = allmat[i]
-        
