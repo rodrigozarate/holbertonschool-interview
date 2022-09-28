c142d4e144106d2679bbf69f4d37069e4846b65f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-09-28 09:32:29 -0500

    [CREATE]

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,14 @@
+#include 
+/**
+ * main - multiplies two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 or 98 on error
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+		error(98);
+
+
+}
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _MUL_H_
+#define _MUL_H_
+
+#include <stddef.h>
+
+int _putchar(char c);
+void error(int status);
+
+#endif /* _MUL_H_ */
