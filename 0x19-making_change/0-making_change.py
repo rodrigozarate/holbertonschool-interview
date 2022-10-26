7b7f574b3580a7ce3c839f5eb0b8954562666d97
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-10-26 15:32:56 -0500

    [UPDATE]

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -7,31 +7,31 @@ Change coin simulation
 
 
 def makeChange(coins, total):
-	""" coins list  of coins, total value to get """
-	if total <= 0:
-		return 0
+    """ coins list  of coins, total value to get """
+    if total <= 0:
+        return 0
 
-	if type(total) is not int or type(coins) is not list:
-		print("Invalid args")
-		return 0
+    if type(total) is not int or type(coins) is not list:
+        print("Invalid args")
+        return 0
 
-	if not all([type(coin) is int for coin in coins]):
-		print("Invalid args")
-		return 0
+    if not all([type(coin) is int for coin in coins]):
+        print("Invalid args")
+        return 0
 
-	coins.sort(reverse=True)
+    coins.sort(reverse=True)
 
-	coin_sum = 0
-	left  = total
-	for coin_val in coins:
-		if coin_val <= left:
-			cant = left // coin_val
-			coin_sum += cant
-			left -= cant * coin_val
-		if left == 0:
-			break
+    coin_sum = 0
+    left = total
+    for coin_val in coins:
+        if coin_val <= left:
+            cant = left // coin_val
+            coin_sum += cant
+            left -= cant * coin_val
+        if left == 0:
+            break
 
-	if coin_sum == 0 or left != 0:
-		return -1
+    if coin_sum == 0 or left != 0:
+        return -1
 
-	return coin_sum
+    return coin_sum
