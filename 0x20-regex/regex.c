c301c6636654072153148a18d6cd322b5b8b9dbc
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-12-14 17:44:03 -0500

    [UPDATE]

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,102 +1,18 @@
 #include "regex.h"
-#include <stdio.h>
-
 
 /**
- * regexPiece - test regex pieces
- * @str: string
- * @pattern: string pattern
- * @str_end: pointer
- * @pttrn_end: pointer
- * Return: 1 or 0 if error
+ * regex_match - matches regex pattern to string
+ * @s: string
+ * @p: pattern
+ * Return: 1 if match else 0
  */
-int regexPiece(char const *str, char const *pattern,
-				const char **str_end, const char **pttrn_end)
+int regex_match(char const *s, char const *p)
 {
-	const char *pttrn_start;
-	int match = 0;
-
-	if (!str || !pattern)
-	{
-		fprintf(stderr, "regexPiece: NULL parameter(s)\n");
-		return (0);
-	}
-
-	while (*str)
-	{
-		while (*str && *str != *pattern)
-			str++;
-
-		pttrn_start = pattern;
-
-		while (*str == *pattern && *pattern != '.' &&
-		       *pattern != '*' && *pattern != '\0' &&
-		       *(pattern + 1) != '*')
-		{
-			str++;
-			pattern++;
-		}
-
-		if (*pattern == '.' || *pattern == '*' ||
-		    *pattern == '\0' || *(pattern + 1) == '*')
-		{
-			*pttrn_end = pattern;
-			*str_end = str;
-			match = 1;
-		}
-
-		pattern = pttrn_start;
-	}
-
-	return (match);
-}
-
-
-/**
- * regex_match - match regex against str
- * @str: string
- * @pattern: pattern
- * Return: 1 or 0 if error
- */
-int regex_match(char const *str, char const *pattern)
-{
-	char repeat;
-	const char *str_end = NULL, *pttrn_end = NULL;
-
-	if (!str || !pattern)
-	{
-		fprintf(stderr, "regex_match: NULL parameter(s)\n");
-		return (0);
-	}
-
-	while (*pattern)
-	{
-		if (*(pattern + 1) == '*')
-		{
-			if (*pattern == '.')
-			{
-				pattern += 2;
-				if (regexPiece(str, pattern, &str_end, &pttrn_end))
-				{
-					str = str_end;
-					pattern = pttrn_end;
-				}
-			}
-			else
-			{
-				repeat = *pattern;
-				pattern += 2;
-				while (*str == repeat)
-					str++;
-			}
-		}
-		else if (*pattern == '.' || *pattern == *str)
-		{
-			pattern++;
-			str++;
-		}
-		else
-			break;
-	}
-	return (!(*str) && !(*pattern));
+	if (!*s)
+		return (!*p || (p[1] == '*' && regex_match(s, p + 2)));
+	if (p[1] == '*')
+		return ((*p == '.' || *s == *p)
+			 ? (regex_match(s + 1, p) || regex_match(s, p + 2))
+			 : (regex_match(s, p + 2)));
+	return ((*p == '.' || *p == *s) && regex_match(s + 1, p + 1));
 }
